b9ebfa45cb614d0039770f85383ec3cbdbbb81cf
Pieter Jan <pieterjaninfo@gmail.com>
2017-01-31 02:33:31 +0100

    Binary puzzle solver nearly perfect, contains small bug

--- src/binarypuzzlesolver.py
+++ src/binarypuzzlesolver.py
@@ -1,6 +1,7 @@
-# Binary puzzle solver for manually (hard-coded) inserted puzzles
+# Binary puzzle solver for manually (hard-coded) inserted puzzles using recursion
 import numpy as np
 
+DMAX = [0, 1]
 bp_size = 6
 bp = np.array([[1, -1, 1, -1, -1, 1],
                [-1, -1, -1, -1, 0, -1],
@@ -10,12 +11,7 @@ bp = np.array([[1, -1, 1, -1, -1, 1],
                [-1, -1, 0, -1, -1, -1]])
 
 
-def copy_grid():
-    array = bp[:]
-    return array
-
-
-def get_free_spot(): #re-implement to be more python like
+def get_free_spot():    # Re-implement to be more python like
     if bp[bp == -1].size == 0:
         return -1
     for i in range(0, bp_size):
@@ -24,60 +20,55 @@ def get_free_spot(): #re-implement to be more python like
                 return [i, j]
 
 
-def row_conflict(i, j, num):
-    row = bp[i]
-    if row[row == num].size >= bp_size/2:
+def array_conflict(array, j, num):
+    if array[array == num].size == bp_size/2:
         return True
-    # if
+    if j == 0 and array[j + 1] == array[j + 2] == num:
+        return True
+    elif j == bp_size - 1 and array[bp_size - 2] == array[bp_size - 3] == num:
+        return True
+    elif j != 0 and j != bp_size - 1 and array[j - 1] == array[j + 1] == num:
+        return True
+    return False
+
+
+def row_conflict(i, j, num):
+    return array_conflict(bp[i], j, num)
+
 
 def col_conflict(i, j, num):
-    col = bp[:,j]
+    return array_conflict(bp[:,j], i, num)
 
 
 def gives_conflict(i, j, num):
-    return row_conflict(i, j, num) and col_conflict(i, j, num)
+    return row_conflict(i, j, num) or col_conflict(i, j, num)
 
-# row_conflict(1,2,0)
-# col_conflict(1,2,0)
 
-index = get_free_spot()
-print(str(index[0]) + ' ' + str(index[1]))
+def solve():
+    if get_free_spot() != -1:
+        index_spot = get_free_spot()
+        row = index_spot[0]
+        col = index_spot[1]
 
-# a = np.array([[1,2,3], [4,5,6], [5,5,9]])
+        for num in DMAX:
+            if not gives_conflict(row, col, num):
+                bp[row, col] = num
+                solve()
+                bp[row, col] = -1
+    else:
+        print(bp)
+        exit(0)
+
+
+print(bp)
+print('')
+solve()
+
+# a=bp[1]
+# b=bp[:,1]
+#
 # print(a)
 # print('')
-# b = a
 # print(b)
-
 #
-# def is_solved():
-#     return bp[bp == -1].size == 0
-
-# def check_opposite():
-#     changes = True
-#     while changes:
-#         changes = False
-#         for i in range(0, bp_size - 2):
-#             for j in range(0, bp_size):
-#                 if bp[i,j] != -1:
-#                     if bp[i,j] == bp[i+2,j] and bp[i+1,j] == -1:
-#                         bp[i+1,j] = get_inverse(bp[i,j])
-#                         changes = True
-#
-#         for i in range(0, bp_size):
-#             for j in range(0, bp_size - 2):
-#                 if bp[i,j] != -1:
-#                     if bp[i,j] == bp[i,j+2] and bp[i,j+1] == -1:
-#                         bp[i,j+1] = get_inverse(bp[i,j])
-#                         changes = True
-#
-#
-# def get_inverse(num):
-#     if num == 0:
-#         return 1
-#     elif num == 1:
-#         return 0
-#
-            #
-# def check_near():
-#     print('hello')
+# print(b[b == 1].size)
